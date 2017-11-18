26fbcf78e5091f160bef9455fc721092179e04cc
nrsyed <nsyed88@gmail.com>
2017-11-17 20:17:30 -0500

    Added animKMeans2D.py and KMeansND.py

---
+++ KMeansND.py
@@ -0,0 +1,95 @@
+import numpy as np
+
+def assignPointsToCentroids(centroids, points):
+    '''Determine the centroid to which each point is nearest, and
+        store this as an int from 0 to K-1 in classifications.
+    '''
+    M = points.shape[0]
+    K = centroids.shape[0]
+    classifications = np.zeros((M,), dtype=np.int)
+
+    for i in range(M):
+        smallestDistance = 0
+        for k in range(K):
+            distance = np.linalg.norm(points[i,:] - centroids[k,:])
+            if k == 0:
+                smallestDistance = distance
+                classifications[i] = k
+            elif distance < smallestDistance:
+                smallestDistance = distance
+                classifications[i] = k
+    return classifications
+        
+def recalcCentroids(centroids, points, classifications):
+    '''Recalculate centroid locations for each cluster.'''
+    K = centroids.shape[0]
+    N = points.shape[1]
+    M = points.shape[0]
+
+    newCentroids = np.zeros((K, N))
+    for k in range(K):
+        if sum(classifications == k) > 0:
+            newCentroids[k,:] = (np.sum(
+                points[classifications == k,:], axis=0) / sum(classifications == k))
+        else:
+            newCentroids[k,:] = centroids[k,:]
+    return newCentroids
+    
+class KMeansND:
+    '''KMeansND(initialCentroids, points)
+
+    PARAMETERS:
+
+    initialCentroids: K x N array of K initial centroids with N features/coordinates.
+
+    points: M x N array of M points with N features/coordinates.
+
+    METHODS:
+
+    (centroids, classifications, iterations) = getCentroids(): Perform K-means
+        clustering. Return a tuple containing the array of centroid coordinates,
+        an M x 1 array of point classifications, and number of iterations required.
+
+    getGenerator(): Return a generator function to step through K-means iterations.
+        Each call of the generator returns the current centroids, classifications,
+        and iteration, beginning with the initial centroids and classifications.
+    '''
+    def __init__(self, initialCentroids, points):
+        if initialCentroids.shape[1] != points.shape[1]:
+            raise RuntimeError('Dimension mismatch. Centroids and data points'
+                + ' must be described by the same number of features.')
+        else:
+            self.initialCentroids = initialCentroids
+            self.points = points
+
+    def getCentroids(self):
+        centroids = np.copy(self.initialCentroids)
+        # Initialize lastCentroids to arbitrary value different from centroids
+        # to ensure loop executes at least once.
+        lastCentroids = centroids + 1
+        iteration = 0
+        while not np.array_equal(centroids, lastCentroids):
+            lastCentroids = np.copy(centroids)
+            classifications = assignPointsToCentroids(centroids, self.points)
+            centroids = recalcCentroids(centroids, self.points, classifications)
+            iteration += 1
+        return (centroids, classifications, iteration)
+
+    def _generatorFunc(self):
+        centroids = np.copy(self.initialCentroids)
+        lastCentroids = centroids + 1
+        iteration = 0
+        initialIteration = True
+        while not np.array_equal(centroids, lastCentroids):
+            if initialIteration:
+                classifications = assignPointsToCentroids(centroids, self.points)
+                initialIteration = False
+            else:
+                lastCentroids = np.copy(centroids)
+                classifications = assignPointsToCentroids(centroids, self.points)
+                centroids = recalcCentroids(centroids, self.points, classifications)
+                iteration += 1
+            yield (centroids, classifications, iteration)
+
+    def getGenerator(self):
+        return self._generatorFunc
---
+++ animKMeans2D.py
@@ -0,0 +1,130 @@
+from __future__ import division
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import colorsys
+import sys
+from datetime import datetime
+from kmeansNd import *
+
+K = 12   # Number of clusters (centroids) to compute
+numClusters = 12     # Number of clusters to generate
+ptsPerCluster = 100
+varianceCoeff = 0.01    # This controls the spread of clustered points
+
+xCenterBounds = (-4, 4)
+yCenterBounds = (-4, 4)
+
+covariance = np.array([[varianceCoeff * (xCenterBounds[1] - xCenterBounds[0]), 0],
+                       [0, varianceCoeff * (yCenterBounds[1] - yCenterBounds[0])]])
+
+def generateClusters():
+    centers = np.random.random_sample((numClusters, 2))
+    centers[:,0] = (
+        centers[:,0] * (xCenterBounds[1] - xCenterBounds[0]) + xCenterBounds[0])
+    centers[:,1] = (
+        centers[:,1] * (yCenterBounds[1] - yCenterBounds[0]) + yCenterBounds[0])
+
+    points = np.zeros((numClusters * ptsPerCluster, 2))
+    for i in range(numClusters):
+        points[i*ptsPerCluster : (i+1)*ptsPerCluster,:] = (
+            np.random.multivariate_normal(centers[i,:], covariance, ptsPerCluster))
+    return points
+
+def initializeCentroids(K, points):
+    '''Randomly select K points as the initial centroid locations'''
+    M = points.shape[0] # number of points
+    indices = []
+    while len(indices) < K:
+        index = np.random.randint(0, M)
+        if not index in indices:
+            indices.append(index)
+    initialCentroids = points[indices,:]
+    return initialCentroids
+
+def animate(clusterInfo):
+    currentCentroids = clusterInfo[0]
+    classifications = clusterInfo[1]
+    iteration = clusterInfo[2]
+    for k in range(K):
+        updatedClusterData = points[classifications == k,:]
+        clusterObjList[k].set_data(updatedClusterData[:,0], updatedClusterData[:,1])
+        centroidObjList[k].set_data(currentCentroids[k,0], currentCentroids[k,1])
+    iterText.set_text('i = {:d}'.format(iteration))
+
+# Create figure and axes. Initialize cluster and centroid line objects.
+fig, ax = plt.subplots()
+clusterObjList = []
+centroidObjList = []
+for k in range(K):
+    clusterColor = tuple(colorsys.hsv_to_rgb(k / K, 0.8, 0.8))
+
+    clusterLineObj, = ax.plot([], [], ls='None', marker='x', color=clusterColor)
+    clusterObjList.append(clusterLineObj)
+
+    centroidLineObj, = ax.plot([], [], ls='None', marker='o',
+        markeredgecolor='k', color=clusterColor)
+    centroidObjList.append(centroidLineObj)
+iterText = ax.annotate('', xy=(0.01, 0.01), xycoords='axes fraction')
+
+def setAxisLimits(ax, points):
+    xSpan = np.amax(points[:,0]) - np.amin(points[:,0])
+    ySpan = np.amax(points[:,1]) - np.amin(points[:,1])
+    pad = 0.05
+    ax.set_xlim(np.amin(points[:,0]) - pad * xSpan,
+        np.amax(points[:,0]) + pad * xSpan)
+    ax.set_ylim(np.amin(points[:,1]) - pad * ySpan,
+        np.amax(points[:,1]) + pad * ySpan)
+
+# Initialize data and K-means clustering. Show and animate plot.
+points = generateClusters()
+initialCentroids = initializeCentroids(K, points)
+genFunc = KMeansND(initialCentroids, points).getGenerator()
+setAxisLimits(ax, points)
+animObj = animation.FuncAnimation(fig, animate, frames=genFunc,
+    repeat=True, interval=500)
+plt.ion()
+plt.show()
+
+# Construct interactive terminal interface.
+inputMessage = ('\nMake a selection:\n'
+    + '(1) Randomize clusters and centroids\n'
+    + '(2) Randomize centroids only\n'
+    + '(3) Save animation to mp4\n'
+    + '(4) Exit\n')
+while 1:
+    if sys.version_info[0] < 3:
+        selection = raw_input(inputMessage)
+    else:
+        selection = input(inputMessage)
+    
+    if selection == '1':
+        animObj._stop()
+        print('\nRandomizing clusters and centroids...')
+        points = generateClusters()
+        initialCentroids = initializeCentroids(K, points)
+        genFunc = KMeansND(initialCentroids, points).getGenerator()
+        setAxisLimits(ax, points)
+        animObj = animation.FuncAnimation(fig, animate, frames=genFunc,
+            repeat=True, interval=500)
+    elif selection == '2':
+        animObj._stop()
+        print('\nRandomizing centroids...')
+        initialCentroids = initializeCentroids(K, points)
+        genFunc = KMeansND(initialCentroids, points).getGenerator()
+        animObj = animation.FuncAnimation(fig, animate, frames=genFunc,
+            repeat=True, interval=500)
+        fig.canvas.draw()
+    elif selection == '3':
+        time = datetime.now()
+        timeStr = (str(time.year) + str(time.month) + str(time.day)
+            + str(time.hour) + str(time.minute) + str(time.second))
+        ffmpegWriterClass = animation.writers['ffmpeg']
+        ffmpegWriterObj = ffmpegWriterClass(fps=1, extra_args=['-vcodec', 'h264'])
+        filename = timeStr + '_KMeans2D.mp4'
+        print('\nSaving file ./' + filename)
+        animObj.save(filename, writer=ffmpegWriterObj)
+    elif selection == '4':
+        exit()
+    else:
+        print('\nInvalid selection.\n')
